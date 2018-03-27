352eb38ee4a230371c77b1ec53baeb35c0af9e14
aHirofumiTakebayashi <hirofumi.takebayashi@access-company.com>
2018-03-27 18:09:17 +0900

    dev_add_concrete_values

--- web/controller/file.ex
+++ web/controller/file.ex
@@ -2,7 +2,16 @@ defmodule Sakura2.Controller.File do
   use SolomonLib.Controller
 
   def create(conn) do
+    %{
+      "DODAI_GROUP_ID" => group_id,
+      "DODAI_ROOT_KEY" => root_key,
+      "DODAI_APP_ID" => app_id
+    } = Sakura2.get_all_env()
     client = Dodai.Client.new(:dev, app_id)
+    filename = "hoge.jpg"
+    content_type = "image/jpeg"
+    size = 100
+    file_collection_name = "sakura2_files"
     body = %Dodai.CreateDedicatedFileEntityRequestBody{filename: filename, contentType: content_type, size: size}
     req  = Dodai.CreateDedicatedFileEntityRequest.new(group_id, file_collection_name, root_key, body)
     res = Dodai.Client.send(client, req)
