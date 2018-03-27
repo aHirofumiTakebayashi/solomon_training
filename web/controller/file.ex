332dc8c8696cadd9df77f165b142dce897c61494
aHirofumiTakebayashi <hirofumi.takebayashi@access-company.com>
2018-03-27 18:10:03 +0900

    fix_response_inspect

--- web/controller/file.ex
+++ web/controller/file.ex
@@ -15,8 +15,8 @@ defmodule Sakura2.Controller.File do
     body = %Dodai.CreateDedicatedFileEntityRequestBody{filename: filename, contentType: content_type, size: size}
     req  = Dodai.CreateDedicatedFileEntityRequest.new(group_id, file_collection_name, root_key, body)
     res = Dodai.Client.send(client, req)
-    Dodai.CreateDedicatedFileEntitySuccess{res_body} = res
-    IO.inspect(res_body)
+    %Dodai.CreateDedicatedFileEntitySuccess{} = res
+    IO.inspect(res)
     json(conn, 201, %{})
   end
 end
